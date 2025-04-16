1036d398b7f4ac5e26bf592524ba6896e33ff27b
Alex Escobar <alexander34357@gmail.com>
2025-04-16 10:49:25 -0500

    Descripción de los cambios realizados: se implemento la llamada de calcular tarifa

---
+++ llamadaTarifa.js
@@ -0,0 +1,5 @@
+const tarifa = require('./CalcularTarifa'); // Asumiendo que lo guardaste como tarifario.js
+
+// Ejemplo de ejecución:
+tarifa.moduloMostrarTarifarioServicios("Lucía", "Gonzales");
+1
